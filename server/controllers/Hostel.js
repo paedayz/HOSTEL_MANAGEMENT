d9644de1186e2b6026e5a25cc0ecc4fce817ffdc
paedayz <pae16phasit@gmail.com>
2021-03-07 19:28:11 +0700

    created update own hostel function in backend

--- server/controllers/Hostel.js
+++ server/controllers/Hostel.js
@@ -4,7 +4,7 @@ const path = require('path')
 // Model
 const Hostel = require('../models/Hostel')
 
-exports.addHostel = (req, res, next) => {
+exports.addHostel = (req, res) => {
     const obj = {
         name: req.body.name,
         price: req.body.price,
@@ -23,4 +23,55 @@ exports.addHostel = (req, res, next) => {
             res.status(200).json({message: 'Add Hostel Success'})
         }
     })
+}
+
+exports.getOwnerUserHostel = (req, res) => {
+    const username = req.user.username
+    
+    Hostel.find({owner: username})
+        .then((hostels) => {
+            console.log(hostels)
+            res.status(200).json({data: hostels})
+        })
+        .catch((err) => {
+            console.log(err)
+            res.status(500).json({error: err})
+        })
+}
+
+exports.editHostel = async (req, res) => {
+    const username = req.user.username
+    const update_data = {}
+
+    if(req.body.name) update_data.name = req.body.name
+    if(req.body.detail) update_data.detail = req.body.detail
+
+    let hostel = await Hostel.findById(req.body._id, (err, data) => {
+        if(err) {
+            res.status(500).json({error: err})
+        } else {
+            if(data) {
+                return data
+            } else {
+                res.status(404).json({error: 'Not found hostel data'})
+            }
+        }
+    })
+
+    if(hostel.owner == username) {
+        Hostel.findOneAndUpdate({_id: req.body._id}, {$set: update_data }, {returnOriginal: false})
+            .then((data) => {
+                let res_data = {
+                    ...data._doc,
+                    ...update_data
+                }
+                res.status(200).json({data: res_data,})
+            })
+            .catch((err) => {
+                console.log(err)
+                res.status(500).json({error: err})
+            })
+    } else {
+        res.status(403).json({error: 'Permission Denied'})
+    }
 }
--- server/routes/hostel.js
+++ server/routes/hostel.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const UserAuth = require('../middlewares/UserAuth')
-const {addHostel} = require('../controllers/Hostel')
+const {addHostel, getOwnerUserHostel, editHostel} = require('../controllers/Hostel')
 
 // Router
 router.post('/addHostel', UserAuth, addHostel)
+router.get('/getOwnerUserHostel', UserAuth, getOwnerUserHostel)
+router.post('/editHostel', UserAuth, editHostel)
 
 module.exports = router
